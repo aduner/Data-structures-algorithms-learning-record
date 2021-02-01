77e1fe99c548e9e95895c0e78c0b927763f53fef
aduner <famin854001260@gmail.com>
2021-02-01 13:38:12 +0800

    2021.2.1

---
+++ Java/src/L012_整数转罗马数字/Solution.java
@@ -0,0 +1,21 @@
+package L012_整数转罗马数字;
+
+public class Solution {
+    private int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    public String intToRoman(int num) {
+        String result="";
+        for(int i=0;i<values.length;i++){
+            if(values[i]<=num){
+                int n=num/values[i];
+                num%=values[i];
+                for(int j=0;j<n;j++){
+                    result+=symbols[i];
+                }
+            }
+
+        }
+        return result;
+    }
+}
