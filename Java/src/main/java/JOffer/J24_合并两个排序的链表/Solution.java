0a82eedd1e0b45c3e4266451e4c972c4c58255fe
aduner <famin854001260@gmail.com>
2021-04-01 10:40:15 +0800

    2021.4.1

---
+++ Java/src/main/java/JOffer/J24_合并两个排序的链表/Solution.java
@@ -0,0 +1,32 @@
+package JOffer.J24_合并两个排序的链表;
+
+/**
+ * @author Aduner
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0), cur = dum;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
