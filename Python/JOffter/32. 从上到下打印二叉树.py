62fbb8c018afcbd9e0e51c876ef1b0503e36e6fe
aduner <famin854001260@gmail.com>
2021-04-11 15:42:29 +0800

    2021.4.11

---
+++ Python/JOffter/32. 从上到下打印二叉树.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+        res=[]
+        queue=[]
+        queue.append(root)
+        while queue:
+            node=queue.pop(0)
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
---
+++ temp.py
@@ -0,0 +1,6 @@
+a=0
+b=1
+if b<a or not a:
+    a=b
+
+print(a)
