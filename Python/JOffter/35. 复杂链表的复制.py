e62750771101cf270be58fc774494a52280ccf35
aduner <famin854001260@gmail.com>
2021-04-23 09:37:10 +0800

    4.23

---
+++ Python/JOffter/35. 复杂链表的复制.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    # 字典法
+    def copyRandomList(self, head):
+        if not head:
+            return
+        dic = {}
+        cur = head
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            dic[cur].next = dic.get(cur.next)
+            dic[cur].random = dic.get(cur.random)
+            cur = cur.next
+        return dic[head]
+
+''' 
+# 拼接法
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head: return
+        cur = head
+        # 1. 复制各节点，并构建拼接链表
+        while cur:
+            tmp = Node(cur.val)
+            tmp.next = cur.next
+            cur.next = tmp
+            cur = tmp.next
+        # 2. 构建各新节点的 random 指向
+        cur = head
+        while cur:
+            if cur.random:
+                cur.next.random = cur.random.next
+            cur = cur.next.next
+        # 3. 拆分两链表
+        cur = res = head.next
+        pre = head
+        while cur.next:
+            pre.next = pre.next.next
+            cur.next = cur.next.next
+            pre = pre.next
+            cur = cur.next
+        pre.next = None # 单独处理原链表尾节点
+        return res      # 返回新链表头节点
+'''
