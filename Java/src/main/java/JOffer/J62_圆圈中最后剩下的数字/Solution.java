2c76a4f75ed40a94c9be3e1074f0c94a64225385
aduner <famin854001260@gmail.com>
2021-06-11 13:58:48 +0800

    2021.6.11

---
+++ Java/src/main/java/JOffer/J62_圆圈中最后剩下的数字/Solution.java
@@ -0,0 +1,17 @@
+package JOffer.J62_圆圈中最后剩下的数字;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author Aduner
+ */
+public class Solution {
+    public int lastRemaining(int n, int m) {
+        int f = 0;
+        for (int i = 2; i != n + 1; ++i) {
+            f = (m + f) % i;
+        }
+        return f;
+    }
+}
