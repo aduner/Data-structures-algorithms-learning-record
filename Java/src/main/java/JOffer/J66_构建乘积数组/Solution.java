87d04e4c2a1a84d3db78aae005d8c5498c7c5c79
aduner <famin854001260@gmail.com>
2021-06-21 10:57:25 +0800

    2021.6.21

--- Java/src/main/java/JOffer/J66_构建乘积数组/Solution.java
+++ Java/src/main/java/JOffer/J66_构建乘积数组/Solution.java
@@ -8,13 +8,13 @@ public class Solution {
         if(a.length == 0) return new int[0];
         int[] b = new int[a.length];
         b[0] = 1;
-        int tmp = 1;
+        int temp = 1;
         for(int i = 1; i < a.length; i++) {
             b[i] = b[i - 1] * a[i - 1];
         }
         for(int i = a.length - 2; i >= 0; i--) {
-            tmp *= a[i + 1];
-            b[i] *= tmp;
+            temp *= a[i + 1];
+            b[i] *= temp;
         }
         return b;
     }
