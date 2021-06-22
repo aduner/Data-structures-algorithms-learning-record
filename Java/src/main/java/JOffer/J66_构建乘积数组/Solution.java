8d368dbdbe23b3eaf74111939f91528c45353295
aduner <famin854001260@gmail.com>
2021-06-22 11:29:48 +0800

    2021.6.22

--- Java/src/main/java/JOffer/J66_构建乘积数组/Solution.java
+++ Java/src/main/java/JOffer/J66_构建乘积数组/Solution.java
@@ -6,16 +6,16 @@ package JOffer.J66_构建乘积数组;
 public class Solution {
     public int[] constructArr(int[] a) {
         if(a.length == 0) return new int[0];
-        int[] b = new int[a.length];
-        b[0] = 1;
+        int[] result = new int[a.length];
+        result[0] = 1;
         int temp = 1;
         for(int i = 1; i < a.length; i++) {
-            b[i] = b[i - 1] * a[i - 1];
+            result[i] = result[i - 1] * a[i - 1];
         }
         for(int i = a.length - 2; i >= 0; i--) {
             temp *= a[i + 1];
-            b[i] *= temp;
+            result[i] *= temp;
         }
-        return b;
+        return result;
     }
 }
